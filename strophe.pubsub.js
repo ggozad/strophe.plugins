c27b3a26f62d01d212cb7806e44f418915133d1f
Lee Boynton <lee@lboynton.com>
2012-07-19 16:55:52 +0100

    Parse numbers when converting from Atom to JSON

--- strophe.pubsub.js
+++ strophe.pubsub.js
@@ -319,12 +319,17 @@
         _AtomToJson: function (xml) {
             var json = {},
                 self = this,
-                jqEl;
+                jqEl,
+                val;
 
             $(xml).children().each(function (idx, el) {
                 jqEl = $(el);
                 if (jqEl.children().length === 0) {
-                    json[el.nodeName.toLowerCase()] = jqEl.text();
+                    val = jqEl.text();
+                    if ($.isNumeric(val)) {
+                        val = Number(val);
+                    }
+                    json[el.nodeName.toLowerCase()] = val;
                 } else {
                     json[el.nodeName.toLowerCase()] = self._AtomToJson(el);
                 }
