591dfc23d7aaa49de265a57851bfeac1f43a6888
Lee Boynton <lee@lboynton.com>
2012-07-19 16:47:45 +0100

    Lowercase node name so that JSON keys are not uppercase

--- strophe.pubsub.js
+++ strophe.pubsub.js
@@ -324,9 +324,9 @@
             $(xml).children().each(function (idx, el) {
                 jqEl = $(el);
                 if (jqEl.children().length === 0) {
-                    json[el.nodeName] = jqEl.text();
+                    json[el.nodeName.toLowerCase()] = jqEl.text();
                 } else {
-                    json[el.nodeName] = self._AtomToJson(el);
+                    json[el.nodeName.toLowerCase()] = self._AtomToJson(el);
                 }
             });
             return json;
